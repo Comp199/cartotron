f607d71bef75e9ed2c7cf3114add960458d8112e
andrea <andrea.czar@gmail.com>
2016-04-23 19:11:45 -0700

    Add missing invoice fk

--- shop/models.py
+++ shop/models.py
@@ -69,6 +69,7 @@ class Invoice(models.Model):
 
 class LineItem(models.Model):
 
+    invoice = models.ForeignKey('Invoice', related_name='line_items')
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
