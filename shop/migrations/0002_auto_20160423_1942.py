e1283cbacfdc025c8ada0c2f46d2b140028adc83
andrea <andrea.czar@gmail.com>
2016-04-23 19:45:53 -0700

    Migrations

--- shop/migrations/0002_auto_20160423_1942.py
+++ shop/migrations/0002_auto_20160423_1942.py
@@ -9,7 +9,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('shop', '0001_squashed_0003_auto_20160423_1935'),
     ]
 
     operations = [
