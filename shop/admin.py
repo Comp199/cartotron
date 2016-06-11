439a6f779647501d17a8b5c32dfc8c3a3d94b19b
Megan <meganwells@shaw.ca>
2016-06-10 19:19:41 -0700

    added carousel model

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from shop.models import Category, Product, Cart, CartItem, Invoice, LineItem
+from shop.models import Category, Product, Cart, CartItem, Invoice, LineItem, Carousel
 
 # Register your models here.
 
@@ -30,8 +30,13 @@ class LineItemInline(admin.TabularInline):
 class InvoiceAdmin(admin.ModelAdmin):
     inlines = (LineItemInline, )
 
+
+class CarouselAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Cart, CartAdmin)
 admin.site.register(CartItem, CartItemAdmin)
 admin.site.register(Invoice, InvoiceAdmin)
+admin.site.register(Carousel, CarouselAdmin)
