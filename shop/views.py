705a5c46914c68a97af1cc2bcdb98300d973d844
Hansen <b.hansen336@gmail.com>
2016-06-14 09:16:13 -0700

    Adding multiple items from product detail page issue has been fixed

--- shop/views.py
+++ shop/views.py
@@ -107,9 +107,9 @@ def cart_add(request, product_id):
 
     if request.method == "POST":
 
-        quantity = 1;
+        quantity = 1
         if 'quantity' in request.POST:
-            quantity = int(request.POST['quantity', 1])
+            quantity = int(request.POST['quantity'])
 
         product = Product.objects.get(id=product_id)
         request.cart.add_item(product, quantity)
